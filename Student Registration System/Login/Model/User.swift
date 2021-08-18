3731cf5156e4b0e18d4c29021434bd757f28da28
Shohan <shohan2@gmail.com>
2021-08-19 02:30:32 +0600

    User model Created.

---
+++ Student Registration System/Login/Model/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Student Registration System
+//
+//  Created by Shohan Rahman on 8/19/21.
+//
+
+import Foundation
+
+struct LoginResponse: Decodable {
+    let status: Bool?
+    let message: String?
+    let token: String?
+    let data: LoginData?
+}
+
+struct LoginData: Decodable {
+    let user: User?
+}
+
+struct User: Decodable {
+    let id: Int?
+    let name: String?
+    let role: String?
+    let email: String?
+}
