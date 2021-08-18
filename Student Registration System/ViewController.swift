6917ee246770f92a07fb0e11caff621025a8342d
Shohan <shohan2@gmail.com>
2021-08-19 00:18:24 +0600

    Project Created.

---
+++ Student Registration System/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student Registration System
+//
+//  Created by Shohan Rahman on 8/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
